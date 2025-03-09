48c82c75311a12864108abd3e231cd20c15ad48e
kaitlynnchan <kaitlynnchan@outlook.com>
2025-03-08 16:58:06 -0800

    integrate gemini call

--- api/schema.py
+++ api/schema.py
@@ -1,6 +1,9 @@
 import graphene
+from .controller.classifier import analyze_prompt_complexity
+import json
 
 class AnalyzeResult(graphene.ObjectType):
+    success = graphene.Boolean(default_value=True)
     query_type = graphene.String()
     url = graphene.String()
 
@@ -8,10 +11,18 @@ class Query(graphene.ObjectType):
     analyze = graphene.Field(AnalyzeResult, prompt=graphene.String(required=True))
 
     def resolve_analyze(self, info, prompt):
-        # Here you can add the logic to process the prompt and access another file
-        # For demonstration, let's assume the processing is successful and return a URL
-        query_type = 'Simple'
-        url = "http://example.com/result"
-        return AnalyzeResult(query_type=query_type, url=url)
+        result_json = analyze_prompt_complexity(prompt)
+        result_json = result_json.replace('\n', '').replace('json','').replace('```', '')
+        result = json.loads(result_json)
+        if 'classification' not in result:
+            return AnalyzeResult(success=False)
+
+        query_type = result['classification']
+        if query_type == 'COMPLEX':
+            return AnalyzeResult(success=True, query_type=query_type)
+        elif result['classification'] == 'SIMPLE':
+            url = result['google_search_url']
+            return AnalyzeResult(success=True, query_type=query_type, url=url)
+        return AnalyzeResult(success=False)
 
 schema = graphene.Schema(query=Query)
